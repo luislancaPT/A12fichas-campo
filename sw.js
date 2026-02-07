40c7d0eb93a25f7d236fafe1c070418167e57c54
luislancaPT <luislancaportugal@gmail.com>
2026-02-07 16:04:43 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,64 +1,19 @@
-// Service Worker for Fichas de Campo PWA
-const CACHE_NAME = 'fichas-campo-v3';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+var CACHE = 'fichas-v5';
+var FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
-// Install
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Cache opened');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
   self.skipWaiting();
 });
 
-// Fetch
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Return cached version or fetch
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(function(response) {
-          // Don't cache non-successful responses
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          // Clone and cache
-          var responseToCache = response.clone();
-          caches.open(CACHE_NAME).then(function(cache) {
-            cache.put(event.request, responseToCache);
-          });
-          return response;
-        });
-      }).catch(function() {
-        // Offline fallback
-        return caches.match('./index.html');
-      })
-  );
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) {
+    return r || fetch(e.request).catch(function() { return caches.match('./index.html'); });
+  }));
 });
 
-// Activate
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== CACHE_NAME;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(names) {
+    return Promise.all(names.filter(function(n) { return n !== CACHE; }).map(function(n) { return caches.delete(n); }));
+  }));
 });
